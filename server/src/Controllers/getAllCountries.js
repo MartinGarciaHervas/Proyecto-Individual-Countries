2ae21787a4770e9fb685e49d9faeb5cf846b6aa7
Martin Garcia Hervas <punighervas@gmail.com>
2023-09-24 00:18:33 -0300

    BackEnd Terminado?

--- server/src/Controllers/getAllCountries.js
+++ server/src/Controllers/getAllCountries.js
@@ -1,12 +1,20 @@
 const {Country} = require('../db')
+const {Op} = require('sequelize')
 
 const getAllCountries = async(req, res) =>{
     try {
         const {name} = req.query
+
+        //Si me llegan Querys, buscamos el pais con el nombre que nos pide, sin ser estricto mediante el iLike
         if(name){
-            const countries = await Country.findAll({where:{name:name}})
-            return res.status(200).json(countries)
+            const countries = await Country.findAll({where:{name:{[Op.iLike]:`%${name}%`}}})
+            if(countries.length){
+                return res.status(200).json(countries);    
+            } 
+            return res.status(404).json('No se enontro un pais con ese nombre:(')   
         }
+
+        //En caso de que no hayan Querys, buscamos todos los paises
         const countries = await Country.findAll()
         return res.status(200).json(countries)
     } catch (error) {
--- server/src/Controllers/getCountrieById.js
+++ server/src/Controllers/getCountrieById.js
@@ -1,12 +1,24 @@
-const {Country} = require('../db');
+const { Country, Activity } = require('../db');
 
-const getCountryById = async(req, res)=>{
+const getCountryById = async (req, res) => {
     try {
-        const {idPais} = req.params;
-        const pais = await Country.findOne({where: {id: idPais}})
-        res.status(200).json(pais)
+        const { idPais } = req.params;
+        const pais = await Country.findOne({ where: { id: idPais } })
+
+        
+        const allActivities = await pais.getActivities()
+        const activities = allActivities.map(activity => (
+            {
+                name: activity.name,
+                difficulty: activity.difficulty,
+                duration: activity.duration,
+                season: activity.season
+            }))
+
+
+        res.status(200).json({pais, activities})
     } catch (error) {
-        res.status(500).json({error: error.message})
+        res.status(500).json({ error: error.message })
     }
 }
 
