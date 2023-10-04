720a34385c58caa776d993c10f71cc891b6bff69
Martin Garcia Hervas <punighervas@gmail.com>
2023-10-03 22:40:17 -0300

    Filtros y Orden OK

--- client/src/views/home/home.jsx
+++ client/src/views/home/home.jsx
@@ -28,57 +28,69 @@ export default function Home() {
         dispatch(filterByActivity(event.target.value))
     }
 
-    const [aux, setAux] = useState(true)
+    const [filterAux, setFilterAux] = useState(true)
 
-    function auxHandler() {
-        setAux(aux ? false : true)
+    function filterAuxHandler() {
+        setFilterAux(filterAux ? false : true)
+    }
+
+    const [orderAux, setOrderAux] = useState(false)
+
+    function orderAuxHandler() {
+        setOrderAux(orderAux ? false : true)
     }
 
     return (
         <>
             <div className={style.home}>
                 <div className={style.order}>
-                    {aux ? <button className={style.button} onClick={auxHandler}><span className="material-symbols-outlined">filter_alt_off</span></button>
-                        : <button className={style.button} onClick={auxHandler}><span className="material-symbols-outlined">filter_alt</span></button>}
-                    {<div className={aux ? style.order2 : style.order3}>
-                        <div>
-                            <p>By population</p>
-                            <select onChange={orderByPopulationHandler}>
-                                <option value='ascendente'>Ascendente</option>
-                                <option value='descendente'>Descendente</option>
-                            </select>
+                    <div className={style.filtros}>
+                        {orderAux ? <button className={style.button} onClick={orderAuxHandler}><span className="material-symbols-outlined">filter_alt_off</span></button>
+                            : <button className={style.button} onClick={orderAuxHandler}><span className="material-symbols-outlined">filter_alt</span></button>}
+                        {<div className={orderAux ? style.order2 : style.order3}>
+                            <div>
+                                <p>By population</p>
+                                <select onChange={orderByPopulationHandler}>
+                                    <option value='ascendente'>Ascendente</option>
+                                    <option value='descendente'>Descendente</option>
+                                </select>
+                            </div>
+                            <div>
+                                <p>Alphabetic</p>
+                                <select onChange={orderByAlphabeticHandler}>
+                                    <option value='ascendente'>Ascendente</option>
+                                    <option value='descendente'>Descendente</option>
+                                </select>
+                            </div>
+                        </div>}
+                    </div>
+                    <div className={style.filtros2}>
+                        <div className={filterAux ? style.ordenes1 : style.ordenes2}>
+                            <div>
+                                <p>By Continent</p>
+                                <select onChange={filterByContinentHandler}>
+                                    <option value=''>All</option>
+                                    <option value='Asia'>Asia</option>
+                                    <option value='North America'>North America</option>
+                                    <option value='South America'>South America</option>
+                                    <option value='Antarctica'>Antarctica</option>
+                                    <option value='Oceania'>Oceania</option>
+                                    <option value='Europe'>Europe</option>
+                                    <option value='Africa'>Africa</option>
+                                </select>
+                            </div>
+                            <div>
+                                <p>By Activity</p>
+                                <select onChange={filterByActivityHandler}>
+                                    <option value=''>All</option>
+                                    {[...new Set(activities.map(activity => activity.name.toUpperCase()))].map(name => (
+                                        <option key={name} value={name}>{name}</option>
+                                    ))}
+                                </select>
+                            </div>
                         </div>
-                        <div>
-                            <p>Alphabetic</p>
-                            <select onChange={orderByAlphabeticHandler}>
-                                <option value='ascendente'>Ascendente</option>
-                                <option value='descendente'>Descendente</option>
-                            </select>
-                        </div>
-                        <div>
-                            <p>By Continent</p>
-                            <select onChange={filterByContinentHandler}>
-                                <option value=''>All</option>
-                                <option value='Asia'>Asia</option>
-                                <option value='North America'>North America</option>
-                                <option value='South America'>South America</option>
-                                <option value='Antarctica'>Antarctica</option>
-                                <option value='Oceania'>Oceania</option>
-                                <option value='Europe'>Europe</option>
-                                <option value='Africa'>Africa</option>
-                            </select>
-                        </div>
-                        <div>
-                            <p>By Activity</p>
-                            <select onChange={filterByActivityHandler}>
-                                <option value=''>All</option>
-                                {[...new Set(activities.map(activity => activity.name.toUpperCase()))].map(name => (
-                                    <option key={name} value={name}>{name}</option>
-                                ))}
-                            </select>
-                        </div>
-
-                    </div>}
+                        <button onClick={filterAuxHandler} className={style.button}><span className="material-symbols-outlined">toc</span></button>
+                    </div>
                 </div>
                 <Paginado />
             </div>
