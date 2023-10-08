a0c93e3ac527f62518c199262f8cc39475ca57ad
Martin Garcia Hervas <punighervas@gmail.com>
2023-10-08 20:30:01 -0300

    Login en proceso

--- client/src/views/login/login.jsx
+++ client/src/views/login/login.jsx
@@ -1,9 +1,33 @@
+import { useEffect, useState } from 'react'
+import {useNavigate} from 'react-router-dom'
+
+//Estilos
 import styles from './login.module.css'
 
 export default function Login(){
+
+    const navigate = useNavigate()
+
+    const [user, setUser] = useState({
+        email: '',
+        password: ''
+    })
+    const [login, setLogin] = useState(false);
+
+    useEffect(()=>{
+        if(login){
+            navigate('/home')
+        }
+    }, [login])
+
+
     return(
         <div>
-            
+            <form>
+                <input value={user.email} placeholder='Email'></input>
+                <input value={user.password} type='password' placeholder='Password'></input>
+                <button type='submit'>Login</button>
+            </form>
         </div>
     )
 }
