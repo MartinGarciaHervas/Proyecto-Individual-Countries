56ba75192a3d3d5efe9f5509bdc14951eb17af5e
Martin Garcia Hervas <punighervas@gmail.com>
2023-10-08 20:59:29 -0300

    Back para register Ready?

---
+++ server/src/Controllers/postUserController.js
@@ -0,0 +1,7 @@
+const {User} = require('../db')
+
+const postUserController=async(user)=>{
+    
+}
+
+module.exports = postUserController
--- server/src/Handlers/postUserHandler.js
+++ server/src/Handlers/postUserHandler.js
@@ -1,8 +1,10 @@
-function postUserHandler(req, res){
+const postUserController = require("../Controllers/postUserController")
+
+const postUserHandler=async(req, res)=>{
     try {
         const user = req.body
-        
-        res.status(200).json({access: true})
+        const response = await postUserController(user)
+        res.status(200).json(response)
     } catch (error) {
         res.status(500).json(error.message)
     }
