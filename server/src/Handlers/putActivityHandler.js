74606b50434003595227f9270f8bbb6b653623b8
Martin Garcia Hervas <punighervas@gmail.com>
2023-10-11 17:03:54 -0300

    Handler ready

--- server/src/Handlers/putActivityHandler.js
+++ server/src/Handlers/putActivityHandler.js
@@ -1,3 +1,13 @@
-const editActivityHandler= async()=>{}
+const editActivityController = require("../Controllers/putActivityController")
+
+const editActivityHandler= async()=>{
+    try {
+        const {name, difficulty, duration, season, CountryId} = req.body
+        await editActivityController(name, difficulty, duration, season, CountryId)
+        res.status(200).json(`${name} editada con exito!!`)
+    } catch (error) {
+        res.status(500).json(error.message)
+    }
+}
 
 module.exports = editActivityHandler
